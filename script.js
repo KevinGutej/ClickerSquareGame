3302f90ea1c039370487c47f83e258d47cd5d081
KevinGutej <76398856+KevinGutej@users.noreply.github.com>
2024-05-04 23:47:31 +0200

    Making the square change colors each time you click it

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const square = document.getElementById('square');
 const container = document.getElementById('container');
 
+// Function to generate a random position
 function getRandomPosition() {
   const containerWidth = container.clientWidth - square.clientWidth;
   const containerHeight = container.clientHeight - square.clientHeight;
@@ -10,12 +11,30 @@ function getRandomPosition() {
   
   return { x: randomX, y: randomY };
 }
+
+// Function to move the square to a random position
 function moveSquare() {
-    const newPosition = getRandomPosition();
-    square.style.left = newPosition.x + 'px';
-    square.style.top = newPosition.y + 'px';
+  const newPosition = getRandomPosition();
+  square.style.left = newPosition.x + 'px';
+  square.style.top = newPosition.y + 'px';
+}
+
+function getRandomColor() {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
   }
-  
-  square.addEventListener('click', moveSquare);
+  return color;
+}
+
+function changeColor() {
+  const newColor = getRandomColor();
+  square.style.backgroundColor = newColor;
+}
+
+square.addEventListener('click', function() {
   moveSquare();
-  
+  changeColor();
+});
+moveSquare();
